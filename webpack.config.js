408a559086f597f8a788b6d9d14fbc1b3bd51a9f
Manuel Victoria <manuelvictoriacode@gmail.com>
2024-04-28 00:23:21 +0200

    feat: new config

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,22 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, 'tsconfig.json'),
-  [/* mapped paths to share */]);
+module.exports = withModuleFederationPlugin({
+  name: "citySights",
 
-module.exports = {
-  output: {
-    uniqueName: "postsCrudChallenge",
-    publicPath: "auto"
+  exposes: {
+    // Update this whole line (both, left and right part):
+    "./PostsCrudChallengeModule":
+      "./src/app/posts-crud-challenge/posts-crud-challenge.module.ts",
   },
-  optimization: {
-    runtimeChunk: false
-  },   
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
-  },
-  experiments: {
-    outputModule: true
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-        library: { type: "module" },
 
-        // For remotes (please adjust)
-        name: "postsCrudChallenge",
-        filename: "remoteEntry.js",
-        exposes: {
-            './PostsCrudChallengeModule': './/src/app/posts-crud-challenge/posts-crud-challenge.module.ts',
-        },        
-        
-        // For hosts (please adjust)
-        // remotes: {
-        //     "mfe1": "http://localhost:3000/remoteEntry.js",
-
-        // },
-
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          "leaflet": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
-          ...sharedMappings.getDescriptors()
-        })
-        
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
     }),
-    sharedMappings.getPlugin()
-  ],
-};
+  },
+});
